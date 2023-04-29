8e508e3e151e97013cf2da1263ba9a22cd02843f
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-29 09:41:49 +0900

    TailwindCSS導入

--- camp/src/App.tsx
+++ camp/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +17,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <button className="bg-black px-4">aa</button>
+
+      <h1>ハレノヒキャンプ</h1>
     </div>
   );
 }
---
+++ camp/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit", // JIT (just-in-time) mode を適用
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
