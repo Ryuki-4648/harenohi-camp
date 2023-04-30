9cd4ca1fb167eec17619828754a70498b302fe1c
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-04-30 13:24:51 +0900

    神戸、東京、京都の5日間の天気を表示させる

--- camp/src/App.tsx
+++ camp/src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import logo from "./logo01.png";
 import "./App.css";
 import axios from "axios";
 
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// ふもとっぱらキャンプ場、グリーンパーク山東、若杉高原おおやキャンプ場
-
 interface WeatherData {
+  city: string;
   date: string;
   minTemp: number;
   maxTemp: number;
@@ -15,34 +13,42 @@ interface WeatherData {
 
 function App() {
   const [weatherData, setWeatherData] = useState<WeatherData[]>([]);
-  const API_KEY = "5403a75c4ed202a7421597001decdcf9"; // 自分のAPIキー
-  const CITY = "Kobe";
+  const API_KEY = "5403a75c4ed202a7421597001decdcf9";
+  const placeArray = [
+    { name: "Kobe", lat: "34.69", lon: "135.19" },
+    { name: "Tokyo", lat: "35.69", lon: "139.69" },
+    { name: "Kyoto", lat: "35.01", lon: "135.75" },
+  ];
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${CITY}&appid=${API_KEY}&units=metric`
-      );
-      console.log(res);
-      const data = res.data.list;
-      const parsedData: WeatherData[] = [];
+      const promises = placeArray.map(async (city) => {
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${API_KEY}&units=metric`
+        );
+        const data = res.data.list;
+        const parsedData: WeatherData[] = [];
 
-      data.forEach((item: any) => {
-        const date = item.dt_txt.split(" ")[0];
+        data.forEach((item: any) => {
+          const date = item.dt_txt.split(" ")[0];
+          const hour = item.dt_txt.split(" ")[1];
+          if (hour === "12:00:00") {
+            parsedData.push({
+              city: city.name,
+              date: date,
+              minTemp: item.main.temp_min,
+              maxTemp: item.main.temp_max,
+              icon: item.weather[0].icon,
+            });
+          }
+        });
 
-        console.log(data);
-        const hour = item.dt_txt.split(" ")[1];
-        if (hour === "12:00:00") {
-          parsedData.push({
-            date: date,
-            minTemp: item.main.temp_min,
-            maxTemp: item.main.temp_max,
-            icon: item.weather[0].icon,
-          });
-        }
+        return parsedData;
       });
 
-      setWeatherData(parsedData);
+      const result = await Promise.all(promises);
+      const mergedData: WeatherData[] = result.flat();
+      setWeatherData(mergedData);
     };
 
     fetchData();
@@ -51,8 +57,13 @@ function App() {
   return (
     <div className="App bg-co">
       <header className="">
-        <a className="" href="/" target="_blank" rel="noopener noreferrer">
-          <img src={logo} className="w-24" alt="logo" />
+        <a
+          className="inline-block w-40"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo} className="w-40" alt="logo" />
         </a>
       </header>
 
@@ -64,6 +75,7 @@ function App() {
       <div>
         {weatherData.map((data, index) => (
           <div key={index}>
+            <h2 className="text-xl">{data.city}</h2>
             <p>{data.date}</p>
             <img
               src={`http://openweathermap.org/img/w/${data.icon}.png`}
